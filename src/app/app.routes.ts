f56e63f3d0ccc39fb150a300a711e910bee5bcc8
JulianBodo <bodojulian01@gmail.com>
2026-09-24 23:23:35 -0300

    Registración de rutas de login y readers, configuración de interceptor

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,13 @@
 import { Routes } from '@angular/router';
-import { Readers } from './readers/readers-list/readers';
+import { BookList } from './books/book-list/book-list';
+import { ReadersList } from './readers/readers-list/readers-list';
+import { Login } from './auth/login/login';
+import { authGuard } from './auth/auth.guard';
+import { LoansList } from './loans/loans-list/loans-list';
 
-export const routes: Routes = [{ path: 'readers', component: Readers }];
+export const routes: Routes = [
+  { path: 'login', component: Login },
+  { path: '', component: BookList, canActivate: [authGuard] },
+  { path: 'readers', component: ReadersList, canActivate: [authGuard] },
+  { path: 'loans', component: LoansList, canActivate: [authGuard] },
+];
